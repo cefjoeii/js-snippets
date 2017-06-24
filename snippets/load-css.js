c298653ecfae16540550954daa3da44ce99267f4
cefjoeii <cefjoeii@gmail.com>
2017-06-24 22:33:44 +0800

    Americanize the word organised

--- snippets/load-css.js
+++ snippets/load-css.js
@@ -1,5 +1,5 @@
 // Load CSS
-// A function the attaches a css link tag on the <head>
+// A function that attaches a css link tag on the <head>
 // Perfect if you only have access to the <body> and want to import a CSS file on the <head>
 // See: https://stackoverflow.com/questions/11833325/css-hack-adding-css-in-the-body-of-a-website
 // loadCSS("path_to_css/file.css");
